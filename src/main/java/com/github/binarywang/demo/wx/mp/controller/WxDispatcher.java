12321c7b1fcbba1ff541d2e761eb807471156ad3
Alex Chew <alex_chew@msn.com>
2019-01-07 09:56:53 +0800

    debug msg

--- src/main/java/com/github/binarywang/demo/wx/mp/controller/WxDispatcher.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/controller/WxDispatcher.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,7 @@ public class WxDispatcher {
 	 */
 	@RequestMapping("/auth")
 	@ResponseBody
-	public WxMpOAuth2AccessToken wxLogin(String code) throws IOException {
+	public WxMpOAuth2AccessToken wxLogin( @RequestParam("code")String code) throws IOException {
 		WxMpOAuth2AccessToken wxMpOAuth2AccessToken = null;
 		try {
 			logger.debug("try to get access token with code and state.",code);
@@ -43,7 +44,7 @@ public class WxDispatcher {
 		return wxMpOAuth2AccessToken;
 	}
 	
-	/**
+	/**we
 	 * 用户授权后获取UserInfo。如果未授权则无信息返回
 	 * 从公众号菜单进入时，回调地址会请求到该服务，并且传递code
 	 * 网页访问则在用户授权后进入本服务
@@ -54,7 +55,7 @@ public class WxDispatcher {
 	 */
 	@RequestMapping("/login")
 	@ResponseBody
-	public WxMpUser WxRedirect(String code) throws WxErrorException, IOException {
+	public WxMpUser WxRedirect( @RequestParam("code")String code) throws WxErrorException, IOException {
 		logger.debug("try to get access token with code and state.",code);
 		//用户同意授权后，通过code获得access token，其中也包含openid
 		WxMpOAuth2AccessToken wxMpOAuth2AccessToken = wxMpService.oauth2getAccessToken(code);
