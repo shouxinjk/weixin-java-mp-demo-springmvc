b1461d0e8308166ea380f24e4b6f7d0c04a4d116
Alex Chew <alex_chew@msn.com>
2023-02-06 22:55:14 +0800

    bug fix

--- src/main/java/com/ilife/util/SxHelper.java
+++ src/main/java/com/ilife/util/SxHelper.java
@@ -339,7 +339,7 @@ public class SxHelper {
 			        .build();
 			List<CompletionChoice> choices = service.createCompletion(completionRequest).getChoices();
 			if(choices!=null && choices.size()>0) {
-				return choices.get(0).getText().replaceAll("\\n","");
+				return choices.get(0).getText().replaceAll("\\n","").replace(keyword+"?","").replace(keyword,"");
 			}
 			return "";
 	  }
