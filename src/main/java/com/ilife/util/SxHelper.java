148d98386dd325f0b888536dc4db5787df800ff9
Alex Chew <alex_chew@msn.com>
2023-02-07 13:11:27 +0800

    minor adjustment

--- src/main/java/com/ilife/util/SxHelper.java
+++ src/main/java/com/ilife/util/SxHelper.java
@@ -339,7 +339,10 @@ public class SxHelper {
 			        .build();
 			List<CompletionChoice> choices = service.createCompletion(completionRequest).getChoices();
 			if(choices!=null && choices.size()>0) {
-				return choices.get(0).getText().replaceAll("\\n","").replace(keyword+"？","").replace(keyword+"?","").replace(keyword,"");
+				return choices.get(0).getText().replaceAll("\\n","")
+						.replace(keyword+"？","").replace(keyword+"?","")
+						.replace(keyword+"：","").replace(keyword+":","")
+						.replace(keyword+" ","");
 			}
 			return "";
 	  }
